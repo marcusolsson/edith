3fc8528846fb9a0b64bd8124894253a722a5976d
Marcus Olsson <olsson.e.marcus@gmail.com>
2020-04-12 20:38:04 +0200

    Improve theme extensions

--- packages/gatsby-theme-edith/src/components/theme.js
+++ packages/gatsby-theme-edith/src/components/theme.js
@@ -3,9 +3,4 @@ import { createMuiTheme } from "@material-ui/core/styles"
 export default theme =>
   createMuiTheme({
     ...theme,
-    palette: {
-      primary: {
-        main: "#ff0000",
-      },
-    },
   })
