b98e4c781e92882483c29aab7c960c49bd43edb9
Marcus Olsson <olsson.e.marcus@gmail.com>
2020-04-12 20:34:40 +0200

    Improve theme extensions

--- packages/gatsby-theme-edith/src/components/BaseLayout.js
+++ packages/gatsby-theme-edith/src/components/BaseLayout.js
@@ -1,4 +1,9 @@
 import React from "react"
+import { createMuiTheme } from "@material-ui/core/styles"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+
+import blue from "@material-ui/core/colors/blue"
+import green from "@material-ui/core/colors/green"
 import { Helmet } from "react-helmet"
 
 import { useStaticQuery, graphql } from "gatsby"
@@ -8,10 +13,92 @@ import GlobalStyles from "./GlobalStyles"
 
 import { ThemeProvider } from "@material-ui/core/styles"
 
-import createTheme from "./theme"
+import customTheme from "./theme"
 
 export default ({ children }) => {
-  const theme = createTheme()
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        appBar: {
+          transparent: true,
+        },
+        overrides: {
+          MuiTable: {
+            root: {
+              marginTop: 14,
+              marginBottom: 14,
+            },
+          },
+        },
+        typography: {
+          fontFamily: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(","),
+          h1: {
+            fontSize: 36,
+            fontWeight: 700,
+            letterSpacing: 0.25,
+            marginBottom: 28,
+          },
+          h2: {
+            fontSize: 28,
+            fontWeight: 700,
+            marginTop: 24,
+            marginBottom: 24,
+          },
+          h3: {
+            fontSize: 24,
+            fontWeight: 700,
+            marginBottom: 20,
+          },
+          h4: {
+            fontSize: 20,
+            fontWeight: 700,
+            marginBottom: 16,
+          },
+          h5: {
+            fontSize: 16,
+            fontWeight: 700,
+            marginBottom: 14,
+            textTransform: "uppercase",
+          },
+          h6: {
+            fontSize: 14,
+            fontWeight: 700,
+            marginBottom: 12,
+            textTransform: "uppercase",
+          },
+
+          body1: {
+            fontSize: 16,
+          },
+          body2: {
+            fontSize: 14,
+          },
+          overline: {
+            fontSize: 16,
+            fontWeight: 500,
+          },
+        },
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: blue,
+          appBar: green,
+        },
+      }),
+    [prefersDarkMode]
+  )
 
   const data = useStaticQuery(
     graphql`
@@ -27,11 +114,13 @@ export default ({ children }) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Helmet title={data.site.siteMetadata.title} />
-      <React.Fragment>
-        <CssBaseline />
-        <GlobalStyles>{children}</GlobalStyles>
-      </React.Fragment>
+      <ThemeProvider theme={customTheme}>
+        <Helmet title={data.site.siteMetadata.title} />
+        <React.Fragment>
+          <CssBaseline />
+          <GlobalStyles>{children}</GlobalStyles>
+        </React.Fragment>
+      </ThemeProvider>
     </ThemeProvider>
   )
 }
--- packages/gatsby-theme-edith/src/components/theme.js
+++ packages/gatsby-theme-edith/src/components/theme.js
@@ -1,92 +1,11 @@
-import React from "react"
 import { createMuiTheme } from "@material-ui/core/styles"
-import useMediaQuery from "@material-ui/core/useMediaQuery"
 
-import blue from "@material-ui/core/colors/blue"
-import green from "@material-ui/core/colors/green"
-
-export default () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
-
-  return React.useMemo(
-    () =>
-      createMuiTheme({
-        appBar: {
-          transparent: true,
-        },
-        overrides: {
-          MuiTable: {
-            root: {
-              marginTop: 14,
-              marginBottom: 14,
-            },
-          },
-        },
-        typography: {
-          fontFamily: [
-            "-apple-system",
-            "BlinkMacSystemFont",
-            '"Segoe UI"',
-            "Roboto",
-            '"Helvetica Neue"',
-            "Arial",
-            "sans-serif",
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-          ].join(","),
-          h1: {
-            fontSize: 36,
-            fontWeight: 700,
-            letterSpacing: 0.25,
-            marginBottom: 28,
-          },
-          h2: {
-            fontSize: 28,
-            fontWeight: 700,
-            marginTop: 24,
-            marginBottom: 24,
-          },
-          h3: {
-            fontSize: 24,
-            fontWeight: 700,
-            marginBottom: 20,
-          },
-          h4: {
-            fontSize: 20,
-            fontWeight: 700,
-            marginBottom: 16,
-          },
-          h5: {
-            fontSize: 16,
-            fontWeight: 700,
-            marginBottom: 14,
-            textTransform: "uppercase",
-          },
-          h6: {
-            fontSize: 14,
-            fontWeight: 700,
-            marginBottom: 12,
-            textTransform: "uppercase",
-          },
-
-          body1: {
-            fontSize: 16,
-          },
-          body2: {
-            fontSize: 14,
-          },
-          overline: {
-            fontSize: 16,
-            fontWeight: 500,
-          },
-        },
-        palette: {
-          type: prefersDarkMode ? "dark" : "light",
-          primary: blue,
-          appBar: green,
-        },
-      }),
-    [prefersDarkMode]
-  )
-}
+export default theme =>
+  createMuiTheme({
+    ...theme,
+    palette: {
+      primary: {
+        main: "#ff0000",
+      },
+    },
+  })
